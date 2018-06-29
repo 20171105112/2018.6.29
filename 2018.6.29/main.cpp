7e6a185c825c7f8363a3c43c2e34392855c7eb29
20171105112 <903056683@qq.com>
2018-06-29 09:59:13 +0800

    自己创建csv格式 。 并且输出表格

--- 2018.6.29/main.cpp
+++ 2018.6.29/main.cpp
@@ -1,15 +1,41 @@
-//
-//  main.cpp
-//  2018.6.29
-//
-//  Created by 黄宏 on 2018/6/29.
-//  Copyright © 2018年 黄宏. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+
+int main()
+{
+    // 写文件
+    ofstream outFile;
+    outFile.open("data.csv", ios::out); // 打开模式可省略
+    outFile << "name" << ',' << "age" << ',' << "hobby" << endl;
+    outFile << "Mike" << ',' << 18 << ',' << "paiting" << endl;
+    outFile << "Tom" << ',' << 25 << ',' << "football" << endl;
+    outFile << "Jack" << ',' << 21 << ',' << "music" << endl;
+    outFile.close();
+    
+    // 读文件
+    ifstream inFile("data.csv", ios::in);
+    string lineStr;
+    vector<vector<string>> strArray;
+    while (getline(inFile, lineStr))
+    {
+        // 打印整行字符串
+        cout << lineStr << endl;
+        // 存成二维表结构
+        stringstream ss(lineStr);
+        string str;
+        vector<string> lineArray;
+        // 按照逗号分隔
+        while (getline(ss, str, ','))
+            lineArray.push_back(str);
+        strArray.push_back(lineArray);
+    }
+    
+    getchar();
     return 0;
 }
